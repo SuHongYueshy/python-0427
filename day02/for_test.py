1c5c4e089e757399ac735d40fc9779ee766ea665
thu <mingfei.net@gmail.com>
2019-04-28 09:41:37 +0800

    Update

--- day02/for_test.py
+++ day02/for_test.py
@@ -1,3 +1,21 @@
-
-for i in range(0, 10000):  #
+for i in range(0, 10):  #
     print(i)
+
+fruits = ['apple', 'banana', 'orange', 'watermelon']  # list 列表
+# fruits = []  # list 列表
+for fruit in fruits:
+    print(fruit)
+    if fruit == 'banana':
+        break
+else:  # 循环正常结束后的处理：没有 break
+    print('else...')
+
+for fruit in fruits:
+    print(fruit)
+    if fruit == 'watermelon':
+        # proceed...
+        print('found watermelon.')
+        break
+# else:  # 循环正常结束后的处理：没有 break
+#     print('not found watermelon.')
+
