029dee1abf4855c00ac4532ddce55b1891de7be9
thu <mingfei.net@gmail.com>
2019-04-28 09:55:54 +0800

    Update

--- day02/for_test.py
+++ day02/for_test.py
@@ -1,7 +1,7 @@
 for i in range(0, 10):  #
     print(i)
 
-fruits = ['apple', 'banana', 'orange', 'watermelon']  # list 列表
+fruits = ['apple', 'banana', 'orange']  # list 列表
 # fruits = []  # list 列表
 for fruit in fruits:
     print(fruit)
@@ -10,12 +10,21 @@ for fruit in fruits:
 else:  # 循环正常结束后的处理：没有 break
     print('else...')
 
+# is_found = False
 for fruit in fruits:
     print(fruit)
     if fruit == 'watermelon':
         # proceed...
         print('found watermelon.')
+        is_found = True
         break
-# else:  # 循环正常结束后的处理：没有 break
+else:  # 循环正常结束后的处理：没有 break
+    print('not found watermelon.')
+
+# if is_found == False:
 #     print('not found watermelon.')
 
+# 语法糖 syntactic sugar: 在没有改变语言功能的前提下，是程序的写法更加简洁
+# 语法： 盐/糖精/海洛因
+
+# 2: for-else
